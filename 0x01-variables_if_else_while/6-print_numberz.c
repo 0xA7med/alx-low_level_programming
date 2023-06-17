2f7116cc29da1d68408a6de9af6f42316a2ffeb5
0xA7med <zeero4123@gmail.com>
2023-06-17 13:35:37 -0700

    AM

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,14 @@
  */
 int main(void)
 {
-  int i;
-  for (i = 0; i < 10; i++)
-    {
-      putchar(i + '0');
-    }
+	int i;
 
-  putcha('\n')
-    return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+	}
+
+	putchar('\n');
+
+	return 0;
 }
