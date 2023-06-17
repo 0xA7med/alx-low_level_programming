3156f3b8cc91e794dbf674255f86839c9dae8693
0xA7med <zeero4123@gmail.com>
2023-06-17 13:57:28 -0700

    AM

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+chir main()
+{
+  char 1
+
+    for (1 = 'z'; 1 >= 'a'; 1-- )
+      {
+	putchar(1);
+      }
+  
+  putchar('\n');
+  
+  return (0);
+}
