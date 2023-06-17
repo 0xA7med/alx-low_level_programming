33c666855184469601e994b9bb7534b2139f49e0
0xA7med <zeero4123@gmail.com>
2023-06-17 06:07:17 -0700

    AM

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    int number_of_languages;  // Number of programming languages
-
-    number_of_languages = 10;
-
-    printf("I know %d programming languages.\n", number_of_languages);
-
+    puts("Programming is like building a multilingual puzzle");
     return 0;
 }
