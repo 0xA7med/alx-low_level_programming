79299cbe0a6cb7545b96e7900f6b87bb21f27d4b
0xA7med <zeero4123@gmail.com>
2023-06-20 08:53:50 -0700

    AM

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <gmp.h>
 
 /**
  * main - Prints the first 98 Fibonacci numbers.
@@ -8,29 +7,21 @@
  */
 int main(void)
 {
-    mpz_t fib1, fib2, fib_next;
-    unsigned long count;
+    unsigned long long fib1 = 1, fib2 = 2, fib_next;
+    unsigned long long count;
 
-    mpz_init(fib1);
-    mpz_init_set_ui(fib2, 1);
-    mpz_init_set_ui(fib_next, 2);
-
-    printf("%lu, %lu", mpz_get_ui(fib2), mpz_get_ui(fib_next));
+    printf("%llu, %llu", fib1, fib2);
 
     for (count = 3; count <= 98; count++)
     {
-        mpz_add(fib_next, fib1, fib2);
-        gmp_printf(", %Zd", fib_next);
+        fib_next = fib1 + fib2;
+        printf(", %llu", fib_next);
 
-        mpz_set(fib1, fib2);
-        mpz_set(fib2, fib_next);
+        fib1 = fib2;
+        fib2 = fib_next;
     }
 
     printf("\n");
-
-    mpz_clear(fib1);
-    mpz_clear(fib2);
-    mpz_clear(fib_next);
 
     return 0;
 }
