0cd0d9f6f307e30cbb139451ea7245bcb00f8d6f
0xA7med <zeero4123@gmail.com>
2023-06-20 09:00:06 -0700

    AM

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	unsigned long long fib1 = 1, fib2 = 2, next, count;
+	unsigned long fib1 = 1, fib2 = 2, next, count;
 
-	printf("%llu, %llu", fib1, fib2);
+	printf("%lu, %lu", fib1, fib2);
 
 	for (count = 3; count <= 98; count++)
 	{
 		next = fib1 + fib2;
-		printf(", %llu", next);
+		printf(", %lu", next);
 
 		fib1 = fib2;
 		fib2 = next;
