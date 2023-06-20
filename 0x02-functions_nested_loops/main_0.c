d618076f2555458525392eeeff56ec4ba465a95b
0xA7med <zeero4123@gmail.com>
2023-06-20 06:35:21 -0700

    AM

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _islower - checks for lowercase character
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
+}
+
+/**
  * test_islower - tests the _islower function
  */
 void test_islower(void)
@@ -9,15 +23,15 @@ void test_islower(void)
     int n;
 
     n = 'H';
-    r = my_islower(n);
+    r = _islower(n);
     _putchar(r + '0');
 
     n = 'o';
-    r = my_islower(n);
+    r = _islower(n);
     _putchar(r + '0');
 
     n = 108; /* ASCII value of 'l' */
-    r = my_islower(n);
+    r = _islower(n);
     _putchar(r + '0');
 
     _putchar('\n');
