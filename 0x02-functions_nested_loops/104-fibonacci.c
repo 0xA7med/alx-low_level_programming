0c074b27f6ea25f7826768de86b4f0d51648cb5c
0xA7med <zeero4123@gmail.com>
2023-06-20 08:59:37 -0700

    AM

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,38 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * add_arrays - Adds two arrays of digits and stores the result in the third array.
- *
- * @a: First array of digits
- * @b: Second array of digits
- * @result: Array to store the sum
- * @size: Size of the arrays
- */
-void add_arrays(int *a, int *b, int *result, int size)
-{
-	int carry = 0;
-
-	for (int i = size - 1; i >= 0; i--)
-	{
-		int sum = a[i] + b[i] + carry;
-		result[i] = sum % 10;
-		carry = sum / 10;
-	}
-}
-
-/**
- * print_array - Prints the elements of an array.
- *
- * @array: Array to print
- * @size: Size of the array
- */
-void print_array(int *array, int size)
-{
-	for (int i = 0; i < size; i++)
-		printf("%d", array[i]);
-}
 
 /**
  * main - Prints the first 98 Fibonacci numbers.
@@ -41,25 +7,17 @@ void print_array(int *array, int size)
  */
 int main(void)
 {
-	const int size = 100;
-	int fib1[size], fib2[size], next[size];
+	unsigned long long fib1 = 1, fib2 = 2, next, count;
 
-	memset(fib1, 0, sizeof(fib1));
-	memset(fib2, 0, sizeof(fib2));
-	memset(next, 0, sizeof(next));
+	printf("%llu, %llu", fib1, fib2);
 
-	fib2[size - 1] = 1;
-
-	printf("1, 2");
-
-	for (int count = 3; count <= 98; count++)
+	for (count = 3; count <= 98; count++)
 	{
-		add_arrays(fib1, fib2, next, size);
-		memcpy(fib1, fib2, sizeof(fib2));
-		memcpy(fib2, next, sizeof(next));
+		next = fib1 + fib2;
+		printf(", %llu", next);
 
-		printf(", ");
-		print_array(next, size);
+		fib1 = fib2;
+		fib2 = next;
 	}
 
 	printf("\n");
