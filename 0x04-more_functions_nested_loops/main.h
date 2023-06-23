d3b5fddd0fd0dbc6fe2ff2457e8f78b2899ccea4
0xA7med <zeero4123@gmail.com>
2023-06-23 14:43:33 -0700

    AM

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,11 @@
 void print_line(int n);
 int _putchar(char c);
 
+void print_square(int size);
+int _putchar(char c);
+
+void print_square(int size);
+int _putchar(char c);
 void print_diagonal(int n);
 int _putchar(char c);
 void more_numbers(void);
