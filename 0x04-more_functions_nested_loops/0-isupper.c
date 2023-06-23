4ebf414a0642af98643004b363cb65741cfa9344
0xA7med <zeero4123@gmail.com>
2023-06-23 14:12:32 -0700

    AM

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * _isupper - Checks if a character is uppercase.
- * @c: The character to be checked.
+ * _isupper - Checks for uppercase character
+ * @c: The character to be checked
  *
- * Return: 1 if the character is uppercase, 0 otherwise.
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    return (c >= 65 && c <= 90);
+    if (c >= 'A' && c <= 'Z')
+        return (1);
+    else
+        return (0);
 }
--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - Check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
